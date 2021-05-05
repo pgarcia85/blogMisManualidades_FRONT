e040742ac1bc12bb7b28be02ad908b22f8846c24
pgarcia85 <villoriaponton@gmail.com>
2021-05-05 19:45:33 +0200

    nueva portada

--- src/componentes/BarraNav.jsx
+++ src/componentes/BarraNav.jsx
@@ -17,7 +17,7 @@ export default class BarraNav extends React.Component {
           }
         return (
             
-                <Nav fill variant="tabs" defaultActiveKey="/posts" className="col-auto">
+                <Nav justify fill variant="tabs" defaultActiveKey="/posts" className="col-md-12 barra">
                     <Nav.Item className="border" >
                         <Nav.Link  href="/sobreMi" eventKey="sobreMi" className="color">Un Poco Sobre Mi</Nav.Link>
                     </Nav.Item>
