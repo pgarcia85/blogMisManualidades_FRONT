ef047de10182b442bdba5d79759fe08a5c692f64
pgarcia85 <villoriaponton@gmail.com>
2021-05-26 13:20:30 +0200

    se añade paginacion al blog

---
+++ src/componentes/Paginacion.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+
+export default class Paginacion extends React.Component {
+
+    render(){
+        const{postPerPage, totalPosts, paginate, nextPage, prevPage}= this.props;
+        const pageNumbers= [];
+        let b =Math.ceil(totalPosts/ postPerPage);
+        for(let i=1; i<= b; i++){
+            pageNumbers.push(i);
+        }
+            return(
+                <nav className="mt-2">
+                <ul className="pagination justifity-content-center">
+                   
+                    {pageNumbers.map(num=>(
+                        <li className="page-item" key={num}>
+                            <a onClick={()=> paginate(num)} className="page-link" href="#">{num}</a>
+                        </li>
+                    ))}
+                   
+                </ul>
+                </nav>
+            )
+
+        
+    }
+
+}
